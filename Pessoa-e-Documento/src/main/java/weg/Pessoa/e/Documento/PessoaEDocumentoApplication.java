0f781122d7dd85fb93588d3dea274811de42be81
Gustavooookefne <gustavooookefne14@gmail.com>
2026-04-24 18:42:01 -0300

    Add files via upload

---
+++ Pessoa-e-Documento/src/main/java/weg/Pessoa/e/Documento/PessoaEDocumentoApplication.java
@@ -0,0 +1,13 @@
+package weg.Pessoa.e.Documento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaEDocumentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaEDocumentoApplication.class, args);
+	}
+
+}
---
+++ Pessoa-e-Documento/src/test/java/weg/Pessoa/e/Documento/PessoaEDocumentoApplicationTests.java
@@ -0,0 +1,13 @@
+package weg.Pessoa.e.Documento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaEDocumentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
